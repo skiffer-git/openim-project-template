2cb2eeab2ce017c96b28c2d36ecc946cb9cff3f1
skiffer-git <44203734@qq.com>
2024-05-16 15:16:21 +0800

    refactor

--- pkg/util/p.go
+++ pkg/util/p.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"github.com/magefile/mage/sh"
 	"os"
 	"os/exec"
@@ -8,21 +9,31 @@ import (
 )
 
 func init() {
-	// 检查并下载protoc-gen-go和protoc-gen-go-grpc插件
-	if _, err := exec.LookPath("protoc-gen-go"); err != nil {
-		sh.Run("go", "install", "google.golang.org/protobuf/cmd/protoc-gen-go@latest")
+	//  protoc, protoc-gen-go 和 protoc-gen-go-grpc
+	tools := map[string]string{
+		"protoc":             "protoc",
+		"protoc-gen-go":      "google.golang.org/protobuf/cmd/protoc-gen-go@latest",
+		"protoc-gen-go-grpc": "google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest",
 	}
-	if _, err := exec.LookPath("protoc-gen-go-grpc"); err != nil {
-		sh.Run("go", "install", "google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest")
+	for tool, path := range tools {
+		if _, err := exec.LookPath(tool); err != nil {
+			fmt.Printf("Installing %s...\n", tool)
+			if err := sh.Run("go", "install", path); err != nil {
+				fmt.Printf("Failed to install %s: %s\n", tool, err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Printf("%s is already installed.\n", tool)
+		}
 	}
 }
 
-// Compile compiles the protobuf files
-func Compile() error {
+// Protocol compiles the protobuf files
+func Protocol() error {
 	protoPath := "./pkg/protocol"
 	dirs, err := os.ReadDir(protoPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read directory: %s", err)
 	}
 
 	for _, dir := range dirs {
@@ -39,15 +50,17 @@ func Compile() error {
 				"--go-grpc_opt=module=" + module,
 				protoFile,
 			}
+			fmt.Printf("Compiling %s...\n", protoFile)
 			if err := sh.Run("protoc", args...); err != nil {
-				return err
+				return fmt.Errorf("failed to compile %s: %s", protoFile, err)
 			}
 
 			// Replace "omitempty" in *.pb.go files
 			files, _ := filepath.Glob(filepath.Join(outputDir, "*.pb.go"))
 			for _, file := range files {
+				fmt.Printf("Fixing omitempty in %s...\n", file)
 				if err := sh.Run("sed", "-i", "", `s/,omitempty\"/\"/g`, file); err != nil {
-					return err
+					return fmt.Errorf("failed to replace omitempty in %s: %s", file, err)
 				}
 			}
 		}
