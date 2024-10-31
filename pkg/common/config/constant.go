599fab94d3f3459a5927250bf93767e83d969548
skiffer-git <44203734@qq.com>
2024-10-31 10:46:32 +0800

    k8s

--- pkg/common/config/constant.go
+++ pkg/common/config/constant.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	MountConfigFilePath     = "MOUNT_CONFIG_FILE_PATH"
-	DeploymentType          = "DEPLOYMENT_TYPE"
+	DeploymentType          = "openim-config"
 	KUBERNETES              = "kubernetes"
 	MongoInitdbRootUsername = "MONGO_INITDB_ROOT_USERNAME"
 	MongoInitdbRootPassword = "MONGO_INITDB_ROOT_PASSWORD"
